eb2a790b1bc17c59746ae0f399a76cdd3bcb56e5
VISLAVATH KRANTHI KIRAN <kranthikiran024@kmit.edu.in>
2026-04-25 12:15:24 +0530

    changes made

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,8 @@ const defaultCorsOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:5175',
-  'http://localhost:80'
+  'http://localhost:80',
+  'https://hire-smart-mu.vercel.app/'
 ];
 
 const allowedOrigins = process.env.CORS_ORIGIN
